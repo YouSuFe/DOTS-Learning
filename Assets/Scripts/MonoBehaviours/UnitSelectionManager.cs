3c13baf849af14d05f263828a4c135abdd52c3b5
Yusuf Mert ÜLGEN <113786181+YouSuFe@users.noreply.github.com>
2025-05-30 19:18:42 +0300

    Add basic selected event system for logic

--- Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
+++ Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
@@ -41,16 +41,18 @@ public class UnitSelectionManager : MonoBehaviour
             EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<Selected>().Build(entityManager);
 
             NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+
+            NativeArray<Selected> selectedArray = entityQuery.ToComponentDataArray<Selected>(Allocator.Temp);
+
             for(int i = 0; i < entityArray.Length; i++)
             {
                 entityManager.SetComponentEnabled<Selected>(entityArray[i], false);
+                Selected selected = selectedArray[i];
+                selected.onDeselected = true;
+                selectedArray[i] = selected;
+                entityManager.SetComponentData(entityArray[i], selected);
             }
-
-
-            entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<LocalTransform, Unit>().WithPresent<Selected>().Build(entityManager);
-
-            entityArray = entityQuery.ToEntityArray(Allocator.Temp);
-            NativeArray<LocalTransform> localTransfromArray = entityQuery.ToComponentDataArray<LocalTransform>(Allocator.Temp);
+            
 
             Rect selectionAreaRect = GetSelectionAreaRect();
 
@@ -60,6 +62,11 @@ public class UnitSelectionManager : MonoBehaviour
 
             if (isMultiplerSelection)
             {
+                entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<LocalTransform, Unit>().WithPresent<Selected>().Build(entityManager);
+
+                entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+                NativeArray<LocalTransform> localTransfromArray = entityQuery.ToComponentDataArray<LocalTransform>(Allocator.Temp);
+
                 for (int i = 0; i < localTransfromArray.Length; i++)
                 {
                     LocalTransform unitLocalTransform = localTransfromArray[i];
@@ -68,6 +75,9 @@ public class UnitSelectionManager : MonoBehaviour
                     if (selectionAreaRect.Contains(unitScreenPosition))
                     {
                         entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+                        Selected selected = entityManager.GetComponentData<Selected>(entityArray[i]);
+                        selected.onSelected = true;
+                        entityManager.SetComponentData(entityArray[i], selected);
                     }
                 }
             }
@@ -97,6 +107,9 @@ public class UnitSelectionManager : MonoBehaviour
                     if(entityManager.HasComponent<Unit>(raycastHit.Entity))
                     {
                         entityManager.SetComponentEnabled<Selected>(raycastHit.Entity, true);
+                        Selected selected = entityManager.GetComponentData<Selected>(raycastHit.Entity);
+                        selected.onSelected = true;
+                        entityManager.SetComponentData(raycastHit.Entity, selected);
                     }
                 }
 
--- Assets/Scripts/System/SelectedVisualSystem.cs
+++ Assets/Scripts/System/SelectedVisualSystem.cs
@@ -2,23 +2,26 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 
+[UpdateInGroup(typeof(LateSimulationSystemGroup))]
+[UpdateBefore(typeof(ResetEventSystem))]
 partial struct SelectedVisualSystem : ISystem
 {
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach(RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>().WithDisabled<Selected>())
+        foreach(RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>().WithPresent<Selected>())
         {
-            RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
-            visualLocalTransform.ValueRW.Scale = 0f;
-        }
-
-        foreach (RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>())
-        {
-            RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
-            visualLocalTransform.ValueRW.Scale = selected.ValueRO.showScale;
+            if(selected.ValueRO.onSelected)
+            {
+                RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
+                visualLocalTransform.ValueRW.Scale = selected.ValueRO.showScale;
+            }
+            else
+            {
+                RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
+                visualLocalTransform.ValueRW.Scale = 0f;
+            }
         }
     }
-
 }
